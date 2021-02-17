7e908bd17268ed722201dc089e3ab9ce13f09b40
Nizar127 <44915278+Nizar127@users.noreply.github.com>
2021-02-17 08:41:20 +0800

    Update Firebase.js

--- config/Firebase.js
+++ config/Firebase.js
@@ -2,13 +2,14 @@ import Firebase from 'firebase';
 
 let firebaseConfig = {
    
-        apiKey: "AIzaSyB1wxkDxROzh1WNktPyq7pHtXVKRS4trxk",
-        authDomain: "traffic-c182a.firebaseapp.com",
-        projectId: "traffic-c182a",
-        storageBucket: "traffic-c182a.appspot.com",
-        messagingSenderId: "594446440423",
-        appId: "1:594446440423:web:c0f98761ecf5dbc1c1cf42",
-        measurementId: "G-D9WDGB6PR0"
+   //Replace the line below only
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
       
 };
 // Initialize Firebase
